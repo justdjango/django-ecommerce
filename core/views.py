66e5c430b10f5106b00b4c6ee77d638c738376a9
Sven Hessenmüller <sven.hessenmueller@gmail.com>
2020-02-10 21:20:44 +0100

    fixes: payment via stripe-api doesn't work

--- core/views.py
+++ core/views.py
@@ -211,7 +211,8 @@ class PaymentView(View):
         if order.billing_address:
             context = {
                 'order': order,
-                'DISPLAY_COUPON_FORM': False
+                'DISPLAY_COUPON_FORM': False,
+                'STRIPE_PUBLIC_KEY' : settings.STRIPE_PUBLIC_KEY
             }
             userprofile = self.request.user.userprofile
             if userprofile.one_click_purchasing:
