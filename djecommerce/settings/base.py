ea49566636f853e7ceb12f0144299ae6b55b07f6
Lucía Parga <luci.parga@hotmail.com>
2020-03-25 17:26:06 +0000

    .

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -13,7 +13,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'django.contrib.sites',
     'allauth',
     'allauth.account',
