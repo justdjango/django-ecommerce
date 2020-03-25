c236a2eb3b27aae138cad9c42768c6b964f3e14c
Lucía Parga <luci.parga@hotmail.com>
2020-03-25 17:03:43 +0000

    .

--- django/lib/python3.7/site-packages/allauth/socialaccount/providers/draugiem/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-
-urlpatterns = [
-    url('^draugiem/login/$', views.login, name="draugiem_login"),
-    url('^draugiem/callback/$', views.callback, name='draugiem_callback'),
-]
--- django/lib/python3.7/site-packages/allauth/socialaccount/providers/openid/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-
-urlpatterns = [
-    url('^openid/login/$', views.login, name="openid_login"),
-    url('^openid/callback/$', views.callback, name='openid_callback'),
-]
