5e9de7489f09a80e1a812a90575b5df26fa71ee0
feiIin <sabrina.chiesurin@gmail.com>
2020-03-25 16:52:28 +0000

    added other questions to the form

--- core/forms.py
+++ core/forms.py
@@ -3,10 +3,15 @@ import pandas as pd
 
 ANSWERS = (('', '-'),('yes', 'Yes'), ('no', 'No'))
 
+INSTRUCTOR = (('', '-'), ('1', '1'), ('2', '2'), ('3', '3'))
+
 
 class FeedbackForm(forms.Form):
+    class_per_week = forms.ChoiceField(choices=INSTRUCTOR)
     timetable = forms.ChoiceField(choices=ANSWERS)
+    instructor = forms.ChoiceField(choices=ANSWERS)
     capacity = forms.ChoiceField(choices=ANSWERS)
     duration = forms.ChoiceField(choices=ANSWERS)
+    class_size = forms.ChoiceField(choices=ANSWERS)
     facilities = forms.ChoiceField(choices=ANSWERS)
     price = forms.ChoiceField(choices=ANSWERS)
