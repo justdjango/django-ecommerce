84581ae7c15d39d903aa893118c18d8c38ef473f
feiIin <sabrina.chiesurin@gmail.com>
2020-04-18 11:34:23 +0100

    added xlrd requirement

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -4,6 +4,7 @@ from decouple import config
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 
+
 SECRET_KEY = config('SECRET_KEY')
 
 INSTALLED_APPS = [
