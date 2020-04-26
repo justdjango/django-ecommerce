2ff48980133e5b7afa8ede2684ae6c0d9575cb37
olebje <ole.bjerkeset@hotmail.com>
2020-04-26 14:17:59 +0200

    feature: add logout message + hotfix: remove old account urls

--- djecommerce/urls.py
+++ djecommerce/urls.py
@@ -5,7 +5,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
     path('', include('core.urls', namespace='core'))
 ]
 
