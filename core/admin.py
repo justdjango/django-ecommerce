340caa3429e7db6fbad4b8287e8f4a67b309d6bb
Mirxoliqov Sayfulla <61648680+MirkhalikovSayfulla@users.noreply.github.com>
2020-10-01 14:45:59 +0100

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -11,45 +11,26 @@ make_refund_accepted.short_description = 'Update orders to refund granted'
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['user',
-                    'ordered',
-                    'being_delivered',
-                    'received',
-                    'refund_requested',
-                    'refund_granted',
-                    'shipping_address',
-                    'billing_address',
-                    'payment',
-                    'coupon'
-                    ]
-    list_display_links = [
-        'user',
-        'shipping_address',
-        'billing_address',
-        'payment',
+    list_display = [
+        'user', 'ordered', 'being_delivered', 'received', 'refund_requested',
+        'refund_granted', 'shipping_address', 'billing_address', 'payment',
         'coupon'
     ]
-    list_filter = ['ordered',
-                   'being_delivered',
-                   'received',
-                   'refund_requested',
-                   'refund_granted']
-    search_fields = [
-        'user__username',
-        'ref_code'
+    list_display_links = [
+        'user', 'shipping_address', 'billing_address', 'payment', 'coupon'
     ]
+    list_filter = [
+        'ordered', 'being_delivered', 'received', 'refund_requested',
+        'refund_granted'
+    ]
+    search_fields = ['user__username', 'ref_code']
     actions = [make_refund_accepted]
 
 
 class AddressAdmin(admin.ModelAdmin):
     list_display = [
-        'user',
-        'street_address',
-        'apartment_address',
-        'country',
-        'zip',
-        'address_type',
-        'default'
+        'user', 'street_address', 'apartment_address', 'country', 'zip',
+        'address_type', 'default'
     ]
     list_filter = ['default', 'address_type', 'country']
     search_fields = ['user', 'street_address', 'apartment_address', 'zip']
