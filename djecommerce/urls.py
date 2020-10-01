5d029f2686d1a1bc373eee4809cd480bb688182a
Mirxoliqov Sayfulla <61648680+MirkhalikovSayfulla@users.noreply.github.com>
2020-10-01 14:46:40 +0100

    Update urls.py

--- djecommerce/urls.py
+++ djecommerce/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,7 +11,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    import debug_toolbar
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
