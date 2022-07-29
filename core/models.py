70492679adbb8a7c165e819a79749fe620ac0750
dneirac <dneirac@unsa.edu.pe>
2022-07-29 18:57:49 -0500

    cambiando categorias

--- core/models.py
+++ core/models.py
@@ -7,9 +7,9 @@ from django_countries.fields import CountryField
 
 
 CATEGORY_CHOICES = (
-    ('S', 'Shirt'),
-    ('SW', 'Sport wear'),
-    ('OW', 'Outwear')
+    ('Z', 'Zapatos'),
+    ('B', 'Botas'),
+    ('NZ', 'Zapatos Niños')
 )
 
 LABEL_CHOICES = (
